ed8d0dff2be532855cb90cd3274ca8f25561c537
nicholas <n1ckorish14@gmail.com>
2021-12-22 11:22:42 +1100

    finished models, index relationships defined

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products have one Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "productTag",
+  foreignKey: "tag_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "productTag",
+  foreignKey: "product_id"
+});
 
 module.exports = {
   Product,
